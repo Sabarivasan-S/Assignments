faea7fc8b0eb8c040cb300c228aa9786a083bee8
Sabarivasan-S <129489290+Sabarivasan-S@users.noreply.github.com>
2023-04-12 17:40:00 +0530

    Update express:assignment_4.js

--- express:assignment_4.js
+++ express:assignment_4.js
@@ -8,97 +8,98 @@ const app = express();
 // Middleware to parse JSON data in request bodies
 app.use(express.json());
 
-// data for students
-const stud = [  { id: 1, name: 'name1' },  { id: 2, name: 'name2' },  { id: 3, name: 'name3' }];
+// Data for students
+const students = [
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Jane Doe' },
+  { id: 3, name: 'Bob Smith' }
+];
 
 // Route to handle the home page
 app.get('/', (req, res) => {
-  res.send('Welcome to home!!!');
+  res.send('Welcome to the Student API!');
 });
 
 // Route to get all the students
 app.get('/api/students', (req, res) => {
-  res.send(stud);
+  res.send(students);
 });
 
 // Route to get a specific student by ID
 app.get('/api/students/:id', (req, res) => {
-  const s = check(req.params.id);
-  if (!s) res.status(404).send(`No students exist with id ${req.params.id}`);
-  else res.send(s);
+  const student = findStudentById(req.params.id);
+  if (!student) {
+    res.status(404).send(`The student with ID ${req.params.id} was not found.`);
+  } else {
+    res.send(student);
+  }
 });
 
 // Route to add a new student
-app.post('/api/students/', (req, res) => {
+app.post('/api/students', (req, res) => {
   // Validate the request body using Joi schema
-  const { error } = validate(req.body);
+  const { error } = validateStudent(req.body);
   if (error) {
     res.status(400).send(error.details[0].message);
     return;
   }
 
-  // Create a new student object and add it to the dummy data array
-  const stu = {
-    id: stud.length + 1,
+  // Create a new student object and add it to the students array
+  const student = {
+    id: students.length + 1,
     name: req.body.name
   };
-  stud.push(stu);
-
-  // Send the newly created student object as response
-  res.send(stu);
+  students.push(student);
+  res.send(student);
 });
 
 // Route to update a student by ID
 app.put('/api/students/:id', (req, res) => {
   // Validate the request body using Joi schema
-  const { error } = validate(req.body);
+  const { error } = validateStudent(req.body);
   if (error) {
     res.status(400).send(error.details[0].message);
     return;
   }
 
   // Find the student by ID and update its name
-  let condition= check(req.params.id);
-  if (!condition) {
-    res.status(404).send('Id not found');
+  let student = findStudentById(req.params.id);
+  if (!student) {
+    res.status(404).send(`The student with ID ${req.params.id} was not found.`);
     return;
   }
-  name= { name: req.body.name };
-
-  // Send the updated student object as response
-  res.send(name);
+  student.name = req.body.name;
+  res.send(student);
 });
 
 // Route to delete a student by ID
 app.delete('/api/students/:id', (req, res) => {
-  // Find the student by ID and remove it from the dummy data array
-  const s = check(req.params.id);
-  if (!s) {
-    res.status(404).send(`The student with id ${req.params.id} not found`);
+  // Find the student by ID and remove it from the students array
+  const student = findStudentById(req.params.id);
+  if (!student) {
+    res.status(404).send(`The student with ID ${req.params.id} was not found.`);
     return;
   }
-  const index = stud.indexOf(s);
-  stud.splice(index, 1);
-
-  // Send success message as response
-  res.send('Delete success');
+  const index = students.indexOf(student);
+  students.splice(index, 1);
+  res.send('The student was deleted successfully.');
 });
 
 // Joi schema to validate the request body for adding/updating a student
-function validate(stu) {
+function validateStudent(student) {
   const schema = Joi.object({
-    name: Joi.string().min(4).required()
+    name: Joi.string().min(3).required()
   });
-  return schema.validate(stu);
+  return schema.validate(student);
 }
 
-// Helper function to find a student by ID in the dummy data array
-function check(stu) {
-  return stud.find(c => c.id === parseInt(stu));
+// Helper function to find a student by ID in the students array
+function findStudentById(id) {
+  return students.find(student => student.id === parseInt(id));
 }
 
 // Starting the server on port 3000
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server active on port ${port}`);
+  console.log(`Server listening on port ${port}`)
 });
