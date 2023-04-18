c23d8cedbee17dc3ed1312e90fd496a719694cde
Sabarivasan-S <129489290+Sabarivasan-S@users.noreply.github.com>
2023-04-18 08:53:13 +0530

    Delete assignment 5 directory

--- assignment 5/createdb.js
+++
@@ -1,6 +0,0 @@
-const Sequelize= require("sequelize");
-let seq=new Sequelize('usersandorders','root','admin',{
-    host:'127.0.0.1',
-    dialect:'postgres'
-});
-seq.query('CREATE DATABASE student').then(()=>{console.log('db created')});
